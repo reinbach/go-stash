96a2609dfc34d47f71762805cf2c8e8e93396421
Greg Reinbach <greg@reinbach.com>
2014-08-13 16:15:42 -0400

    added get repos list method

--- stash/repos.go
+++ stash/repos.go
@@ -2,6 +2,7 @@ package stash
 
 import (
 	"fmt"
+	"net/url"
 )
 
 type Project struct {
@@ -9,14 +10,48 @@ type Project struct {
 }
 
 type Repo struct {
-	Name    string `"json:name"`
-	Slug    string `"json:slug"`
-	Public  bool   `"json:public"`
-	Project *Project
+	Name    string   `"json:name"`
+	Slug    string   `"json:slug"`
+	Public  bool     `"json:public"`
+	ScmId   string   `"json:scmId"`
+	Project *Project `"json:project"`
+}
+
+type Response struct {
+	Values        []Repo `"json:values"`
+	Size          int    `"json:size"`
+	NextPageStart int    `"json:nextPageStart"`
+	IsLastPage    bool   `"json:isLastPage"`
 }
 
 type RepoResource struct {
 	client *Client
+}
+
+// Get list of repositories
+func (r *RepoResource) List() ([]Repo, error) {
+	repos := []Repo{}
+	response := Response{}
+	path := "/repos"
+	params := url.Values{}
+
+	for {
+		if response.NextPageStart != 0 {
+			params.Set("start", fmt.Sprintf("%d", response.NextPageStart))
+		}
+
+		if err := r.client.do("GET", "core", path, params, nil, &response); err != nil {
+			return nil, err
+		}
+
+		repos = append(repos, response.Values...)
+
+		if response.IsLastPage {
+			break
+		}
+	}
+
+	return repos, nil
 }
 
 // Get the named repository
