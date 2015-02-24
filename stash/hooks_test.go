74974d92a101bba1eacc3b64336e8c4e16d013e5
Greg Reinbach <greg@reinbach.com>
2015-02-24 09:31:42 -0500

    Added more hook tests, for invalid projects and links

--- stash/hooks_test.go
+++ stash/hooks_test.go
@@ -5,12 +5,13 @@ import (
 	"testing"
 )
 
-var (
-	link = "link"
-)
-
 func GetHook() string {
-	return fmt.Sprintf("%s?owner=%s&name=%s&branch=${refChange.name}&hash=${refChange.toHash}&message=${refChange.type}&author=", testURL, testProject, testRepo)
+	return fmt.Sprintf(
+		"%s?owner=%s&name=%s&branch=${refChange.name}&hash=${refChange.toHash}&message=${refChange.type}&author=",
+		testURL,
+		testProject,
+		testRepo,
+	)
 }
 
 func TestCreateHook(t *testing.T) {
@@ -23,9 +24,36 @@ func TestCreateHook(t *testing.T) {
 	}
 }
 
+func TestCreateHookInvalidLink(t *testing.T) {
+	hook, err := client.Hooks.CreateHook(testProject, testRepo, hookKey, "link")
+	if err == nil {
+		t.Error("Expected error on `client.Hooks.CreateHook()`")
+	}
+	if hook != nil {
+		t.Errorf("Did not expect hook data, got %v", hook)
+	}
+}
+
+func TestCreateHookInvalidProject(t *testing.T) {
+	hook, err := client.Hooks.CreateHook("wrong", testRepo, hookKey, GetHook())
+	if err == nil {
+		t.Error("Expected error on `client.Hooks.CreateHook()`")
+	}
+	if hook != nil {
+		t.Errorf("Did not expect hook data, got %v", hook)
+	}
+}
+
 func TestDeleteHook(t *testing.T) {
 	err := client.Hooks.DeleteHook(testProject, testRepo, hookKey, GetHook())
 	if err != nil {
 		t.Errorf("Unexpected error on `client.Hooks.DeleteHook()`, got %v", err)
+	}
+}
+
+func TestDeleteHookInvalidProject(t *testing.T) {
+	err := client.Hooks.DeleteHook("wrong", testRepo, hookKey, GetHook())
+	if err == nil {
+		t.Error("Expected error on `client.Hooks.DeleteHook()`")
 	}
 }
